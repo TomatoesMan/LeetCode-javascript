6c22be8d575180f9a841c9c6778e37677833cd2a
TomatoesMan <765148684@qq.com>
2021-06-27 18:42:11 +0800

    fix: 142.环形链表 || 中代码

--- 链表/142.环形链表 II/142.环形链表 II.js
+++ 链表/142.环形链表 II/142.环形链表 II.js
@@ -11,26 +11,22 @@
  * @return {ListNode}
  */
  var detectCycle = function(head) {
-  if  (head === null) {return null};
-  let slow = head;
-  let fast = head;
-  while (slow && fast) {
-      if (fast.next !== null) {
-          fast = fast.next.next;
-      } else {
-          return null;
-      }
-      slow = slow.next;
-      if (slow === fast) {
-          let ptr = head;
-          while(ptr && slow) {
-              if (ptr === slow) {
-                  return ptr;
-              }
-              ptr = ptr.next;
-              slow = slow.next;
-          }
-      }
-  }
-  return null;
+    if  (head === null) {return null};
+    let slow = head;
+    let fast = head;
+    while (slow && fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            let ptr = head;
+            while(ptr && slow) {
+                if (ptr === slow) {
+                    return ptr;
+                }
+                ptr = ptr.next;
+                slow = slow.next;
+            }
+        }
+    }
+    return null;
 };
