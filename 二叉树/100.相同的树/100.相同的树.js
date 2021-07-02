e6c85aa461484bf8e91f2e01889c34495a5dd249
yaoxl <yaoxl@autohome.com.cn>
2021-07-02 11:00:11 +0800

    feat: 100.相同的树

---
+++ 二叉树/100.相同的树/100.相同的树.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+ var isSameTree = function(p, q) {
+  const dfs = (p, q) => {
+      if (!p && !q) {
+          return true;
+      } else if (!p || !q) {
+          return false;
+      } else if (p.val !== q.val) {
+          return false;
+      } else {
+          return dfs(p.left, q.left) && dfs(p.right, q.right); 
+      }
+  }
+  return dfs(p, q);
+};
