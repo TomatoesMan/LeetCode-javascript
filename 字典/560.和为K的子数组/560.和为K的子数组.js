da0aa03495a94c75c448b8fef56d51bb884a464c
TomatoesMan <765148684@qq.com>
2021-07-29 10:56:45 +0800

    feat: 560.和为k的子数组

---
+++ 字典/560.和为K的子数组/560.和为K的子数组.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+ var subarraySum = function(nums, k) {
+  let map = new Map();
+  map.set(0, 1);
+  let count = 0, pre = 0;
+  for (let num of nums) {
+      pre += num;
+      if (map.has(pre - k)) {
+          count += map.get(pre - k);
+      } 
+      if (map.has(pre)) {
+          map.set(pre, map.get(pre) + 1);
+      } else {
+          map.set(pre, 1);
+      }
+  }
+  return count;
+};
