12a0e51629d0a230c1ff80d2d933c7791e176dcf
yaoxl <yaoxl@autohome.com.cn>
2021-08-19 11:03:04 +0800

    feat: 374.猜数字大小

---
+++ 二分查找/374.猜数字大小/374.猜数字大小.js
@@ -0,0 +1,27 @@
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	            -1 if num is lower than the guess number
+ *			             1 if num is higher than the guess number
+ *                       otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var guessNumber = function(n) {
+  let low = 0;
+  let height = n;
+  while (low <= height) {
+      let mid = Math.floor((low + height) / 2);
+      if (guess(mid) === -1) {
+          height = mid - 1;
+      } else if (guess(mid) === 1) {
+          low = mid + 1;
+      } else {
+          return mid;
+      }
+  }
+};
